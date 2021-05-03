37904b18a19e88c4391e64a7d9be0df092e69a78
LeHaine <colt.daily@gmail.com>
2021-05-03 19:55:45 -0400

    add controller test

--- src/commonMain/kotlin/com/lehaine/lab/TestScene.kt
+++ src/commonMain/kotlin/com/lehaine/lab/TestScene.kt
@@ -1,24 +1,74 @@
 package com.lehaine.lab
 
+import com.lehaine.kiwi.korge.InputController
 import com.lehaine.kiwi.korge.view.createEnhancedSpriteAnimation
 import com.lehaine.kiwi.korge.view.enhancedSprite
 import com.lehaine.kiwi.korge.view.getEnhancedSpriteAnimation
 import com.soywiz.klock.milliseconds
 import com.soywiz.klock.seconds
+import com.soywiz.korev.GameButton
 import com.soywiz.korev.Key
 import com.soywiz.korge.input.keys
 import com.soywiz.korge.scene.Scene
 import com.soywiz.korge.view.Container
+import com.soywiz.korge.view.addUpdater
 import com.soywiz.korge.view.position
 import com.soywiz.korge.view.scale
 import com.soywiz.korim.atlas.readAtlas
 import com.soywiz.korio.file.std.resourcesVfs
 
-class LevelScene : Scene() {
+sealed class Input {
+    object MoveHorizontal : Input()
+    object MoveVertical : Input()
+}
+
+class TestScene : Scene() {
 
     override suspend fun Container.sceneInit() {
         val atlas = resourcesVfs["tiles.atlas.json"].readAtlas()
+        val controller = InputController<Input>(views)
 
+        controller.addAxis(
+            Input.MoveHorizontal,
+            listOf(Key.D),
+            listOf(GameButton.LX),
+            listOf(Key.A),
+            listOf(GameButton.LX)
+        )
+        val sprite = enhancedSprite {
+            smoothing = false
+            playAnimationLooped(atlas.createEnhancedSpriteAnimation("heroRun") {
+                frames(0..1, frameTime = 2.seconds)
+                frames(2..3, frameTime = 500.milliseconds)
+                frames(1..2, frameTime = 100.milliseconds)
+            })
+            scale(5, 5)
+            position(50, 50)
+        }
+
+        addUpdater {
+            controller.update()
+            val moveDir = controller.strength(Input.MoveHorizontal)
+            if (controller.down(Input.MoveHorizontal)) {
+                sprite.x += moveDir * 3
+            }
+        }
+
+
+        keys {
+            down(Key.ESCAPE) {
+                stage?.views?.debugViews = false
+                stage?.gameWindow?.run {
+                    debug = false
+                    close()
+                }
+            }
+        }
+    }
+
+
+    private suspend fun Container.testEnhancedSprites() {
+        val atlas = resourcesVfs["tiles.atlas.json"].readAtlas()
         enhancedSprite {
             smoothing = false
             playAnimationLooped(atlas.createEnhancedSpriteAnimation("heroRun") {
@@ -52,16 +102,6 @@ class LevelScene : Scene() {
             }
             scale(5, 5)
             position(150, 50)
-        }
-
-        keys {
-            down(Key.ESCAPE) {
-                stage?.views?.debugViews = false
-                stage?.gameWindow?.run {
-                    debug = false
-                    close()
-                }
-            }
         }
     }
 }
--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,4 +1,4 @@
-import com.lehaine.lab.LevelScene
+import com.lehaine.lab.TestScene
 import com.soywiz.korge.Korge
 import com.soywiz.korge.scene.Module
 import com.soywiz.korim.color.Colors
@@ -9,13 +9,13 @@ import com.soywiz.korma.geom.SizeInt
 suspend fun main() = Korge(Korge.Config(module = GameModule))
 
 object GameModule : Module() {
-    override val mainScene = LevelScene::class
+    override val mainScene = TestScene::class
 
     override val windowSize: SizeInt = SizeInt(Size(480 * 3, 270 * 3))
     override val size: SizeInt = SizeInt(Size(480, 270))
     override val bgcolor = Colors["#2b2b2b"]
 
     override suspend fun AsyncInjector.configure() {
-        mapPrototype { LevelScene() }
+        mapPrototype { TestScene() }
     }
 }
