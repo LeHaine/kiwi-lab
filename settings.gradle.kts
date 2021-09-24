42d10e2098a927b03b0c0ac02dd2c73bef6962a9
LeHaine <colt.daily@gmail.com>
2021-09-24 12:29:00 -0400

    update korge to 2.4.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,10 @@
 pluginManagement {
     repositories {
         mavenLocal()
-        maven("https://jitpack.io")
-        maven("https://dl.bintray.com/korlibs/korlibs")
-        maven("https://plugins.gradle.org/m2/")
         mavenCentral()
         google()
+        maven("https://jitpack.io")
+        maven("https://plugins.gradle.org/m2/")
     }
 
     val korgePluginVersion: String by settings
